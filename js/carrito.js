12175fb6f9721e3c586692e4c51aa11c50d96618
Werjman Matias <139645562+matiasjaw80@users.noreply.github.com>
2023-10-30 23:16:47 -0300

    Coments

--- js/carrito.js
+++ js/carrito.js
@@ -1,16 +1,19 @@
 let productosEnCarrito = localStorage.getItem("productos-en-carrito");
 productosEnCarrito = JSON.parse(productosEnCarrito);
-
+//QuerySelector
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
 const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+
+
 let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+//QuerySelector
 const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
 const botonComprar = document.querySelector("#carrito-acciones-comprar");
 
-
+//Functions
 function cargarProductosCarrito() {
     if (productosEnCarrito && productosEnCarrito.length > 0) {
 
